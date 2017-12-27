40d4f586c3874f7b3c6c286383fb9efc37bcdf55
Misha Zharov <mishazharov1@gmail.com>
2017-12-26 21:20:53 -0500

    More ui work, should merge this now

--- app/src/main/java/com/mishazharov/alarmrace/MainActivity.java
+++ app/src/main/java/com/mishazharov/alarmrace/MainActivity.java
@@ -3,7 +3,9 @@ package com.mishazharov.alarmrace;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.util.Log;
+import android.widget.EditText;
 
 import org.json.JSONException;
 
@@ -15,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle(getString(R.string.main_title));
+
+        // Makes sure that challenge code is typed in all caps. Just an aesthetic thing
+        ((EditText) findViewById(R.id.challenge_code)).setFilters(new InputFilter[]{new InputFilter.AllCaps()});;
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
